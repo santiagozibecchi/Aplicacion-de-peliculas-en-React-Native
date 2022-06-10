f7f71566b5bb36d9f7f006ed018bd47d622673e5
santiagozibecchi <103424513+santiagozibecchi@users.noreply.github.com>
2022-06-09 21:23:28 -0300

    Update movieDB.tsx

--- src/api/movieDB.tsx
+++ src/api/movieDB.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const movieDB = axios.create({
      baseURL: 'https://api.themoviedb.org/3/movie',
      params: {
-          api_key: '8486241eff6dea37e13e87312ef184a1',
+          api_key: '',
           language: 'es-ES',
      },
 });
